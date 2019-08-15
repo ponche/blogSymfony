5a9bce4a6162d89d57228a2c4671fd09285a92a4
Rémi <ponche62880@gmail.com>
2019-08-15 16:15:55 +0200

    create form

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,7 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request; 
+use Doctrine\Common\Persistence\ObjectManager; 
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use App\Entity\Article; 
+use App\Form\ArticleType;
 
 class BlogController extends AbstractController
 {
@@ -29,15 +36,53 @@ class BlogController extends AbstractController
      */
     public function home()
     {
-        return $this->render('blog/home.html.twig'); 
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+
+        $articles = $repo->findAll();
+
+        return $this->render('blog/home.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $articles
+        ]);
     }
 
     /**
      * @Route("/blog/create", name="blog_create")
      */
-    public function create()
+    public function create(Request $request, ObjectManager $manager)
     {
-        return $this->render('blog/create.html.twig') ; 
+        dump($request); 
+        $article = new Article() ; 
+
+        // 1eme méthode pour crée un formulaire
+        // $form = $this->createFormBuilder($article)
+        //             ->add('title')
+        //             ->add('content')
+        //             ->add('image')
+        //             ->add('save', SubmitType::class, [
+        //                 'label' => 'Enregister', 
+        //             ])
+        //             ->getForm();
+
+        // 2eme méthode beaucoup plus rapide 
+        $form = $this->createForm(ArticleType::class, $article);
+
+        // analyse la requete 
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid() )
+        {
+
+            $article->setCreatedAt(new \DateTime());
+
+            $manager->persist($article);
+
+            $manager->flush();
+        }
+        return $this->render('blog/create.html.twig', [
+            'form' => $form->createView(),
+        ]) ; 
     }
 
     /**
@@ -48,7 +93,7 @@ class BlogController extends AbstractController
         $repo = $this->getDoctrine()->getRepository(Article::class); 
 
         $article = $repo->find($id);  
-        
+
         return $this->render('blog/show.html.twig', [
             'article' =>$article , 
         ]) ; 
